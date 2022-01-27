2caee8e69faed6c2c2b360d5167369c282fa0c09
frano.saric@diplomat.ba <frano.saric@diplomat.ba>
2022-01-27 13:09:10 +0100

    changing get method for books

--- app/routes/books.routes.js
+++ app/routes/books.routes.js
@@ -3,28 +3,28 @@ const { authJwt } = require("../middleware");
 const controller = require("../controllers/books.controller");
 
 module.exports = function (app) {
-    app.use(function (req, res, next) {
-        res.header(
-            "Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept"
-        );
-        next();
-    });
-
-    app.get(
-        "/getBooks",
-        [authJwt.verifyToken, /*authJwt.isEmployee*/],
-        controller.getBooks
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
     );
-    app.post(
-        "/createBook",
-        [authJwt.verifyToken, /*authJwt.isEmployee*/],
-        controller.createBook
-    );
+    next();
+  });
 
-    app.delete(
-      "/deleteBook",
-      [authJwt.verifyToken, /*authJwt.isEmployee*/],
-        controller.deleteBook
-    )
+  app.post(
+    "/getBooks",
+    [authJwt.verifyToken /*authJwt.isEmployee*/],
+    controller.getBooks
+  );
+  app.post(
+    "/createBook",
+    [authJwt.verifyToken /*authJwt.isEmployee*/],
+    controller.createBook
+  );
+
+  app.delete(
+    "/deleteBook",
+    [authJwt.verifyToken /*authJwt.isEmployee*/],
+    controller.deleteBook
+  );
 };
